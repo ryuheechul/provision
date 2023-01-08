dc8ed706b044300eb5bfb59ca4f7b9a263a17f5a
Heechul Ryu <ryuhcii@gmail.com>
2023-01-08 11:22:08 -0800

    lima/tailscale - allow forwarding for agent and x11

--- lima/bin/start-or-resume.sh
+++ lima/bin/start-or-resume.sh
@@ -7,17 +7,21 @@ instance="${LIMA_INSTANCE}"
 
 extracted_name="$(limactl ls --json | jq -r ". | select(.name == \"${instance}\") | .name")"
 
-# if exist, resume
-# if not, start
 if test "${extracted_name}" = "${instance}"; then
+	# if exist, resume
 	limactl start --tty=false "${LIMA_INSTANCE}"
+	# wait how do I make an update to an existing machine?
+	# 1. edit config.yaml for future reiteration
+	# 2. `limactl edit "${LIMA_INSTANCE}"` and apply the same change
+	# 3. verify the changes are made - if it's actually applied is up to lima
 else
+	# if not, start
   config_file_path="./config.yaml"
   # config.yaml will be loaded if exist
 	if test -f "${config_file_path}"; then
 		limactl start --tty=false --name="${LIMA_INSTANCE}" ${config_file_path}
 	else
-		# I know this is weird but it is what it is now as of version lima v0.9.1
+		# I know this is weird but it is what it is for now as of version lima v0.9.1
 		limactl start --tty=false "${LIMA_INSTANCE}"
 	fi
 fi
